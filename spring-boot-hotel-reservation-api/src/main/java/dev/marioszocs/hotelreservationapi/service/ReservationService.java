97961a643f70d75d342d14f455333e0c7480a6a3
tmunkhb <tmunkhb@gmail.com>
2025-05-15 15:32:08 -0400

    Update ReservationService.java

--- spring-boot-hotel-reservation-api/src/main/java/dev/marioszocs/hotelreservationapi/service/ReservationService.java
+++ spring-boot-hotel-reservation-api/src/main/java/dev/marioszocs/hotelreservationapi/service/ReservationService.java
@@ -6,13 +6,66 @@ import dev.marioszocs.hotelreservationapi.entity.Reservation;
 
 import java.util.List;
 
+/**
+ * Service interface for handling business logic related to reservations.
+ */
 public interface ReservationService {
+
+    /**
+     * Retrieves a list of all reservations.
+     *
+     * @return list of {@link Reservation} objects
+     */
     List<Reservation> getAllReservations();
-    Reservation getReservation(Integer id);
-    IdEntity saveReservation(Reservation reservations);
-    SuccessEntity deleteReservation(Integer id);
-    boolean validateHotelExistenceById(Integer id);
+
+    /**
+     * Retrieves a reservation by its ID.
+     *
+     * @param reservationId the ID of the reservation to retrieve
+     * @return the {@link Reservation} object
+     */
+    Reservation getReservation(Integer reservationId);
+
+    /**
+     * Saves or updates the provided reservation.
+     *
+     * @param reservation the {@link Reservation} object to save
+     * @return an {@link IdEntity} containing the ID of the saved reservation
+     */
+    IdEntity saveReservation(Reservation reservation);
+
+    /**
+     * Deletes the reservation with the given ID.
+     *
+     * @param reservationId the ID of the reservation to delete
+     * @return a {@link SuccessEntity} indicating the outcome of the operation
+     */
+    SuccessEntity deleteReservation(Integer reservationId);
+
+    /**
+     * Checks if a hotel exists by the given ID.
+     *
+     * @param hotelId the ID of the hotel
+     * @return true if the hotel exists, false otherwise
+     */
+    boolean validateHotelExistenceById(Integer hotelId);
+
+    /**
+     * Checks whether one date is before another.
+     *
+     * @param date1 the first date (in ISO format)
+     * @param date2 the second date (in ISO format)
+     * @return true if date1 is before date2, false otherwise
+     */
     boolean dateIsBefore(String date1, String date2);
-    boolean reservationOverlaps(Reservation reservations);
-    boolean validateReservationExistence(Integer id);
-}
+
+    /**
+     * Determines if a new reservation overlaps with existing ones.
+     *
+     * @param reservation the reservation to check
+     * @return true if overlapping, false otherwise
+     */
+    boolean reservationOverlaps(Reservation reservation);
+
+    /**
+     * Valid*
